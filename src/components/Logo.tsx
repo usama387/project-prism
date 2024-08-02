0306b58abeff6becd54f397353d2e50d797d126c
usama387 <128306049+usama387@users.noreply.github.com>
2024-08-02 11:40:21 +0500

    MobileNavbar implemented with shadecn ui

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -15,4 +15,16 @@ const Logo = () => {
   );
 };
 
+export function LogoMobile() {
+  return (
+    <div>
+      <Link href="/" className="flex items-center gap-2">
+        <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl leading-tight tracking-tighter text-transparent">
+          Project Prism
+        </p>
+      </Link>
+    </div>
+  );
+}
+
 export default Logo;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,22 @@
 "use client";
 
-import React from "react";
-import Logo from "./Logo";
+import React, { useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { buttonVariants } from "./ui/button";
+import { Button, buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
 import { ThemeSwitcherButton } from "./ThemeSwitcherButton";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+import Logo, { LogoMobile } from "./Logo";
 
 // child component of dashboard layout
 const Navbar = () => {
   return (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
     </>
   );
 };
@@ -62,7 +65,15 @@ function DesktopNavbar() {
 }
 
 // function that accepts item as prop contains link and name
-function NavbarItem({ link, name }: { link: string; name: string }) {
+function NavbarItem({
+  link,
+  name,
+  clickCallback,
+}: {
+  link: string;
+  name: string;
+  clickCallback?: () => void;
+}) {
   // tracking user route
   const pathname = usePathname();
 
@@ -80,12 +91,59 @@ function NavbarItem({ link, name }: { link: string; name: string }) {
           "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
           isActive && "text-foreground"
         )}
+        onClick={() => {
+          if (clickCallback) clickCallback();
+        }}
       >
         {name}
       </Link>
       {isActive && (
         <div className="absolute -bottom-[2px] left-1/2 h-[2px] w-[80%] -translate-x-1/2 rounded-xl bg-foreground md:block" />
       )}
+    </div>
+  );
+}
+
+function MobileNavbar() {
+  // managing opening state of mobile navbar
+  const [open, setOpen] = useState(false);
+
+  // in this function for mobile navbar i am using shadcn ui Sheet , SheetTrigger and SheetContent to implement hamburger menu and then mapping the items once again sending as props to the NavbarItem to be displayed here
+
+  return (
+    <div className="block border-separate bg-background md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        {/* Mobile Menu Sheet */}
+        <Sheet open={open} onOpenChange={setOpen}>
+          <SheetTrigger asChild>
+            <Button variant="ghost" size="icon">
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]" side="left">
+            <Logo />
+            <div className="flex flex-col gap-1 pt-4">
+              {items.map((item) => (
+                <NavbarItem
+                  key={item.id}
+                  link={item.link}
+                  name={item.name}
+                  clickCallback={() => setOpen(!open)}
+                />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        {/* Logo */}
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <LogoMobile />
+        </div>
+        {/* Dark Mode Switch and Clerk User Button */}
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherButton />
+          <UserButton afterSignOutUrl="/sign-in" />
+        </div>
+      </nav>
     </div>
   );
 }
