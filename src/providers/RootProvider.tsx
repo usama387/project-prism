542c5a3876abaa743c33f6c23e1493da3f6c2538
usama387 <128306049+usama387@users.noreply.github.com>
2024-08-12 11:27:11 +0500

    Tanstack query integrated in the app along with devtools

--- src/providers/RootProvider.tsx
+++ src/providers/RootProvider.tsx
@@ -1,18 +1,28 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const RootProvider = ({ children }: { children: ReactNode }) => {
+  // since react query is client side it wraps the whole app as context api
+  const [queryClient] = React.useState(() => new QueryClient({}));
+
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="dark"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="dark"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+
+      {/* devtools are disabled at the beginning */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
