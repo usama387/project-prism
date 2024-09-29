91462d5d141159206a5e1a5489059751d6a68e20
usama387 <128306049+usama387@users.noreply.github.com>
2024-09-29 10:18:22 +0500

    CreateProject route UI and its layout revised

--- src/app/(projects)/CreateProject/layout.tsx
+++ src/app/(projects)/CreateProject/layout.tsx
@@ -1,11 +1,9 @@
-import Navbar from "@/components/Navbar";
 import React, { ReactNode } from "react";
 
 const ProjectsPageLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="relative h-screen w-full flex-col ">
+    <div className="relative h-screen flex flex-col items-center justify-center">
       <div className="w-full">
-        <Navbar />
         {children}
       </div>
     </div>
--- src/app/(projects)/CreateProject/page.tsx
+++ src/app/(projects)/CreateProject/page.tsx
@@ -1,29 +1,55 @@
 import { Button } from "@/components/ui/button";
 import CreateProjectDialog from "../_components/CreateProjectDialog";
+import Logo from "@/components/Logo";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const CreateProject = () => {
+const CreateProject = async () => {
+  // getting user from clerk
+  const user = await currentUser();
+
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return (
-    <div className="container flex flex-col max-w-2xl items-center justify-between mt-4">
+    <div className="container flex max-w-2xl flex-col items-center justify-between gap-4">
       <div>
-        <h1 className="text-center text-3xl font-semibold text-emerald-400">
-          Welcome! Let's Get Started on Your New Project
+        <h1 className="text-center text-3xl">
+          Welcome, <span className="ml-3 font-bold">{user.firstName}! 👋</span>
         </h1>
+        <h2 className="mt-4 text-center text-base text-muted-foreground">
+          Lst &apos;s get started by setting up your first Project
+        </h2>
+        <h3 className="mt-2 text-center text-sm text-muted-foreground">
+          You can add tasks and change settings at any time
+        </h3>
       </div>
-      <div className="h-full bg-background">
-        <div className="border-b bg-card">
-          <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
-            <CreateProjectDialog
-              trigger={
-                <Button
-                  variant={"outline"}
-                  className="border-emerald-300 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
-                >
-                  New Project
-                </Button>
-              }
-            />
-          </div>
+      <Separator />
+      <div className="border-b bg-card">
+        <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+          <CreateProjectDialog
+            trigger={
+              <Button
+                variant={"outline"}
+                className="border-emerald-300 bg-emerald-950 text-white hover:bg-emerald-700 hover:text-white"
+              >
+                New Project
+              </Button>
+            }
+          />
         </div>
+      </div>
+      <Separator />
+      <Button className="w-full" asChild>
+        <Link href="/projects">
+          I&apos;m done! Take me to the Projects Page
+        </Link>
+      </Button>
+      <div className="mt-8">
+        <Logo />
       </div>
     </div>
   );
