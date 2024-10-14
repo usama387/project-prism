2f7fe8a3b8fad803ad76be783ee0e6751010cb5f
usama387 <128306049+usama387@users.noreply.github.com>
2024-10-14 16:55:31 +0500

    Navbar doubled and removed it then

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,7 +5,6 @@ const DashboardLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="relative h-screen w-full flex-col ">
       <div className="w-full">
-        <Navbar />
         {children}
       </div>
     </div>
