03886a90f02890265e507cd061c78c16d78fbe77
usama387 <128306049+usama387@users.noreply.github.com>
2024-10-14 17:38:17 +0500

    Removed auto top margin

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear()
 
   return (
-    <footer className="mt-auto px-8">
+    <footer className="px-8">
       <div className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           <div className="text-center sm:text-left">
