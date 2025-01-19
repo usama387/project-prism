1f417ef0b81e218e46c20d8d1bf2f81f8b84635b
usama387 <128306049+usama387@users.noreply.github.com>
2025-01-19 12:22:04 +0500

    Removed where condition to fetch all projects

--- src/app/api/my-projects/route.ts
+++ src/app/api/my-projects/route.ts
@@ -1,20 +1,9 @@
 import prisma from "@/lib/prisma";
-import { currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
 
 // fetches projects belonging to a particular user
 export const GET = async () => {
-  const user = await currentUser();
-
-  if (!user) {
-    redirect("/sign-in");
-  }
-
-  const userProjects = await prisma.project.findMany({
-    where: {
-      userId: user.id,
-    },
+  const allProjects = await prisma.project.findMany({
     include: {
       tasks: {
         select: {
@@ -37,5 +26,5 @@ export const GET = async () => {
 
   revalidatePath("/MyProjects");
 
-  return Response.json(userProjects);
+  return Response.json(allProjects);
 };
