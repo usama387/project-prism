72cce3cc460117bda0a8d46eaf8e71f3becf7a7f
usama387 <128306049+usama387@users.noreply.github.com>
2025-03-17 19:30:03 +0500

    Specified all protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,20 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// protected routes in the app
-const isProtectedRoute = createRouteMatcher(["/"]);
+// protected routes in the app are specified here
+const isProtectedRoute = createRouteMatcher([
+  "/",
+  "/MyProjects",
+  "/ProjectChart",
+  "/CreateProject",
+  "/MyTasks",
+  "/MyTasks/versions",
+  "/CreateTask",
+  "/TasksChart",
+  "/Issues",
+  "/Announcements",
+  "/transactions",
+  "/manage",
+]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
